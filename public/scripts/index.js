1adda639559f0cf4802a8ab34d83620777b556d4
Ricardo Magalhães <ricardo_energie@hotmail.com>
2020-04-18 18:52:24 +0100

    fixed minor issue

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,17 +1,18 @@
 var infoPlaceholders = [$('#location'), $('#temperature'), $('#time')];
 var gif = 'https://media0.giphy.com/media/tXL4FHPSnVJ0A/giphy.gif?cid=ecf05e4714223db933ff48ea74ac87d46eb0d627e56e685e&rid=giphy.gif';
 var ptFlag = 'https://www.countryflags.io/pt/shiny/64.png';
-var location = $('#input-address').val();;
 
-$('form').on('submit', e => {
+$('form').on('submit', (e) => {
 
     e.preventDefault();
 
+    var location = $('#input-address');
+
     loading();
 
-    fetch('/weather?address=' + location).then(response => {
+    fetch('/weather?address=' + location.val()).then(response => {
 
-        clearTextBox();
+        location.val('');
 
         response.json().then(data => data.error || data.length == 0 ? renderError(data) : renderInfo(data))
             .catch(error => $('#location').text(error))
@@ -23,8 +24,6 @@ var loading = () => {
     $('#nation-flag').attr('src', gif);
     infoPlaceholders.forEach(info => info.text(''));
 };
-
-var clearTextBox = () => locationInput.val('');
 
 var renderError = data => {
     $('#location').text(data.error);
