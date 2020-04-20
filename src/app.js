e7eb44672a37c270e4b6b90e6fe7f07276f01b96
Ricardo Magalhães <ricardo_energie@hotmail.com>
2020-04-20 15:23:40 +0100

    refactoring

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views');
 const partialsPath = path.join(__dirname, '../templates/partials');
 
-//Setup handlears engine and views location
+//Setup handleBars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
 hbs.registerPartials(partialsPath);
